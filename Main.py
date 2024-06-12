380f05e8aa578c6da7109ac5727ca1b7f60ad9c8
Mohamad Yaacoub <152275907+MohmadYaacoub@users.noreply.github.com>
2024-06-12 15:36:18 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from DataPreparation import Dataset
 from ElaborationClass import CNN_1D
 
-dataset = Dataset(base_file_path=r"C:\Users\mhamad.yackoub\OneDrive - unige.it\Desktop\VScode\Multimodal Glove\Csv Fusion")
+dataset = Dataset(base_file_path=r"")
 X_train, y_train, X_val, y_val, X_test, y_test = dataset.get_data_splits()
 
 model = CNN_1D(X_train, y_train, X_val, y_val, X_test, y_test)
