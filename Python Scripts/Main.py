c2eb3ec9f6db966ae8a1d8695db91a00bb3a15a4
Mohamad Yaacoub <152275907+MohmadYaacoub@users.noreply.github.com>
2025-02-07 12:02:22 +0100

    Update Main.py

--- Python Scripts/Main.py
+++ Python Scripts/Main.py
@@ -1,7 +1,7 @@
 from DataPreparation import Dataset
-from ElaborationClass import CNN_1D
+from ElaborationClass import CNN1D
 
 dataset = Dataset(base_file_path="Dataset")
 X_train, y_train, X_val, y_val, X_test, y_test = dataset.get_data_splits()
 
-model = CNN_1D(X_train, y_train, X_val, y_val, X_test, y_test)
+model = CNN1D(X_train, y_train, X_val, y_val, X_test, y_test)
